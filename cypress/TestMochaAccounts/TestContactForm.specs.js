4541cd21f82967311dce558883eae30f79e521ff
Sonam <sonam2686@gmail.com>
2024-10-19 01:38:00 +0530

    test configuration with c;oud

--- cypress/TestMochaAccounts/TestContactForm.specs.js
+++ cypress/TestMochaAccounts/TestContactForm.specs.js
@@ -11,9 +11,9 @@ describe('MOCHA Accounting Contact Form Fail',function(){
  it('Mocha Fail Test Case Form',function(){
 
 cy.get('a').contains('About & Contact').click();
-cy.get('input[name="your-name"]').type('Test Name');
-cy.get('input[name="your-email"]').type('Test@yopmail.com');
-cy.get('input[name="your-company"]').type('Test Subject');
+//cy.get('input[name="your-name"]').type('Test Name');
+//cy.get('input[name="your-email"]').type('Test@yopmail.com');
+//cy.get('input[name="your-company"]').type('Test Subject');
 cy.get('select').select(1);
 cy.get('textarea[name="your-message"]').type('Test Subject');
 cy.get('iframe')
